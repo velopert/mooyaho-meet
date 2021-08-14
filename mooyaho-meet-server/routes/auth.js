cc6362bd3c7dfc1cc13eb0629890dfe7b765ac9a
Minjun Kim <public.velopert@gmail.com>
2021-08-15 00:02:08 +0900

    Working on auth

--- mooyaho-meet-server/routes/auth.js
+++ mooyaho-meet-server/routes/auth.js
@@ -1,12 +1,47 @@
 const fastify = require('fastify')
+const db = require('../db')
+const bcrypt = require('bcrypt')
+
 /**
  * @param {fastify.FastifyInstance} fastify
  * */
 async function authRouter(fastify) {
   fastify.post('/register', async (request, reply) => {
     const { username, password } = request.body
+    // check username exists
+    const exists = await db.user.findUnique({ where: { username } })
+    if (exists) {
+      reply.status(409)
+      throw new Error('Username already exists')
+    }
+    const hash = await bcrypt.hash(password, 10)
+    const user = await db.user.create({
+      data: {
+        username,
+        passwordHash: hash,
+      },
+      select: {
+        id: true,
+        username: true,
+      },
+    })
+    return user
   })
-  fastify.post('/login', (request, reply) => {})
+  fastify.post('/login', async (request, reply) => {
+    const { username, password } = request.body
+    const user = await db.user.findUnique({ where: { username } })
+    if (!user) {
+      reply.status(401)
+      throw new Error('Login failed')
+    }
+    const isMatch = await bcrypt.compare(password, user.passwordHash)
+    if (isMatch) {
+      const { id, username } = user
+      return { id, username } // @todo return token
+    }
+    reply.status(401)
+    throw new Error('Login failed')
+  })
   fastify.post('/integrate', (request, reply) => {})
   fastify.post('/integrate-guest', (request, reply) => {})
 }
